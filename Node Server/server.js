851821119730badc4d9bedc1194a11bd9cb648ae
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2016-11-19 15:50:58 -0500

    Started movie algorithm for choice

--- Node Server/server.js
+++ Node Server/server.js
@@ -176,17 +176,23 @@ db.once('open', function(){
 function renderMovieList(res, user){
     //TODO load top movies and pass them to view
     var Movie = mongoose.model("Movie");
-
-    Movie.find({},null,{limit:20, sort:{
+    var bestMovies = [];
+    Movie.find({},null,{sort:{
         "tomatoUserMeter":1
     }},function(req,movies){
+        for(var i=0; i < movies.length; i++){
+            var mGenres = movies[i].Genre.split(",").trim();
+            for(var k=0; j < user.genres; j++){
+                
+            }
+        }
+
+
         res.render('pages/selectMovies.ejs', {
             user: user,
             movieList: movies
         });
     });
-
-
 }
 function renderGenreList(res, user){
     console.log(user.name);
