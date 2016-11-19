bb0b7db9535ff4f722bd1cba88ad45ef3263157c
Arthur K <arthurkuhn@outlook.com>
2016-11-19 12:13:07 -0500

    Fixed Merge conflicts

--- Node Server/server.js
+++ Node Server/server.js
@@ -14,12 +14,12 @@ require("./models/movie.js");
 // configuration ===============================================================
 mongoose.connect(database.remoteUrl); 	// Connect to local MongoDB instance. A remoteUrl is also available (modulus.io)
 
+
 app.use(express.static('./public')); 		// set the static files location /public/img will be /img for users
 app.use(morgan('dev')); // log every request to the console
 app.use(bodyParser.json()); // parse application/json
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
 app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
-
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
