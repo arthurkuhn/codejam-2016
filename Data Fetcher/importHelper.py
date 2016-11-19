7e0e9de0ac2cc0bf805cd50af5f06388101c989e
Mohamed Kleit <mohamedkleit@wpa168191.wireless.mcgill.ca>
2016-11-19 13:25:54 -0500

    Save tv_show name without date nor ranking

--- Data Fetcher/importHelper.py
+++ Data Fetcher/importHelper.py
@@ -4,14 +4,20 @@ Created on Sat Nov 19 11:27:28 2016
 
 @author: Arthur
 """
+import codecs
 import csv
 
 def fetchShowsList():
     resultList = []
-    with open('movieList.csv', newline='') as csvfile:
+    with codecs.open('movieList.csv', encoding='utf8') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
         for row in spamreader:
-            resultList.append(row)
-            print(', '.join(row))
-
-    return ["The Shining"]
+            #print(row[0])
+            i1 = row[0].find('.')
+            i2 = row[0].find('(')
+            #print(str(i1) + ":" + str(i2))
+            show_name = row[0][i1+2:i2-1]
+            print(show_name)
+            resultList.append(show_name)
+   
+    return resultList
