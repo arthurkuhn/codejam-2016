efd0bfb2ea49821d35584715ce44ec36132fdd0b
Arthur K <arthurkuhn@outlook.com>
2016-11-19 10:03:36 -0500

    Modified Python Script

--- Data Fetcher/script.py
+++ Data Fetcher/script.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import requests
 import csv
+import json
 
 
 def main():
@@ -11,26 +12,32 @@ def main():
     #Pull Data
     title = "Ocean's Eleven"
     query = "http://www.omdbapi.com/?t=" + title + "&y=&plot=long&tomatoes=true&r=json"
-    
     r = requests.get(query)
-    print(r.content)
-    print(r.status_code)
-    print(r.json())
     
-    payload = r.json()
     
-    #Send to DB
-    r = requests.post(mondbPutUrl, data = payload)
+    #print(r.content)
+    #print(r.status_code)
+    #print(r.content)
     
-    print(r.content)
-    print(r.status_code)
+    show = json.loads(r.content.decode("utf-8") )
+    print(json.dumps(show, sort_keys=False,indent=4, separators=(',', ': ')))
+    print(show["Title"])
+    #Send to DB1
+    #r = requests.post(mondbPutUrl, data = payload)
+    
     #Save as a new row in a csv File
+    
+    row = getRow(show)
     
     with open(fileAddress, 'wb') as csvfile:
         spamwriter = csv.writer(csvfile,delimiter = ',', quotechar = '"', doublequote = True, skipinitialspace = True, lineterminator = '\r\n', quoting = csv.QUOTE_MINIMAL)
-        spamwriter.writerow(payload)
+        #spamwriter.writerow(row)
         
     print("done")
+    
+def getRow(jsonObj):
+    #print(jsonObj)
+    return
     
     
     
