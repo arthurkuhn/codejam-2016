5455601b745894f3d2836886c6a0bd3ca608514a
Arthur K <arthurkuhn@outlook.com>
2016-11-19 11:11:44 -0500

    Modularisation of Python Script

---
+++ Data Fetcher/exportHelper.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 19 11:05:18 2016
+
+@author: Arthur
+"""
+import csv
+
+def exportShow(show):
+    #Send to DB
+    addShowToDb(show)
+    
+    #Save as a new row in a csv File
+    addShowToCsv(show)
+    
+
+def addShowToDb(show):
+    mondbPutUrl = "http://localhost:3000/postMovie"
+    #r = requests.post(mondbPutUrl, data = show)
+    
+def addShowToCsv(show):
+    fileAddress = "test.csv"
+    row = getRow(show)
+    with open(fileAddress, 'a', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile,delimiter = ',', quotechar = '"', doublequote = True, skipinitialspace = True, lineterminator = '\r\n', quoting = csv.QUOTE_MINIMAL)
+        spamwriter.writerow(row)
+    
+    
+def getRow(show):
+    valuesToGet = ["Title","Plot","Metascore","imdbVotes","tomatoUserMeter"]
+    row = []
+    for id in valuesToGet:
+        val = show[id]
+        if(is_number(val)):
+            dat = cleanNumber(val)
+        else :
+            dat = val
+        row.append(dat)
+    return row
+    
+    
+def is_number(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        try:
+            str = s.replace(",","")
+            int(str)
+            return True
+        except ValueError:
+            return False
+
+def cleanNumber(number):
+    str = number.replace(",","")
+    return int(str)
+    
--- Data Fetcher/script.py
+++ Data Fetcher/script.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 import requests
-import csv
 import json
+import exportHelper
 
 
 def main():
-    
-    mondbPutUrl = "http://localhost:3000/postMovie"
-    fileAddress = "test.csv"
-    
+
     #Pull Data
     title = "Ocean's Eleven"
     query = "http://www.omdbapi.com/?t=" + title + "&y=&plot=long&tomatoes=true&r=json"
@@ -20,49 +17,10 @@ def main():
     
     show = json.loads(r.content.decode("utf-8") )
     #print(json.dumps(show, sort_keys=False,indent=4, separators=(',', ': ')))
-    
-    
-    #Send to DB
-    #r = requests.post(mondbPutUrl, data = show)
-    
-    #Save as a new row in a csv File
-    row = getRow(show)
-    print(type(row))
-    
-    with open(fileAddress, 'w', newline='') as csvfile:
-        spamwriter = csv.writer(csvfile,delimiter = ',', quotechar = '"', doublequote = True, skipinitialspace = True, lineterminator = '\r\n', quoting = csv.QUOTE_MINIMAL)
-        spamwriter.writerow(row)
-        
-    print("done")
-    
-def getRow(show):
-    valuesToGet = ["Title","Plot","Metascore","imdbVotes","tomatoUserMeter"]
-    row = []
-    for id in valuesToGet:
-        val = show[id]
-        if(is_number(val)):
-            dat = cleanNumber(val)
-        else :
-            dat = val
-        row.append(dat)
-    return row
-    
-    
-def is_number(s):
-    try:
-        int(s)
-        return True
-    except ValueError:
-        try:
-            str = s.replace(",","")
-            int(str)
-            return True
-        except ValueError:
-            return False
 
-def cleanNumber(number):
-    str = number.replace(",","")
-    return int(str)
+    exportHelper.exportShow(show)
+    print("done")
+
 
 if __name__ == "__main__":
     main()
